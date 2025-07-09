ea965f607b6c9fd3b3d5f6715fcb58b0dd27a3d4
Austin Vanloon <wic404learn@gmail.com>
2025-07-09 08:08:20 -0600

    Initial commit boilderplate and variables

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+let secretWord
+let guessedLetters
+let remainingGuesses
+let gameStatus
+
+
+
+
+const wordEl = document.getElementById('word')
+const lettersEl = document.getElementById('letters')
+const messageEl = document.getElementById('message')
+const playAgainBtn = document.getElementById('play-again')
+
+
+function init() {
+    
+}
