529f8503afb355d0127eafa46b6d92891bf7d495
Austin Vanloon <wic404learn@gmail.com>
2025-07-10 15:11:09 -0600

    completed handlemove and render functions

--- js/app.js
+++ js/app.js
@@ -15,5 +15,56 @@ const playAgainBtn = document.getElementById('play-again')
 
 
 function init() {
-    
+    const wordList = ['word1', 'word2', 'word3', 'word4', 'word5']
+    secretWord = wordList[Math.floor(Math.random() * wordList.length)]
+    guessedLetters = []
+    remainingGuesses = 6
+    gameStatus = 'active'
+    // console.log('Secret word:', secretWord)
+    // render()
+}
+
+
+function render() {
+    let displayWord = ''
+    for (let char of secretWord) {
+        displayWord += guessedLetters.includes(char) ? char : '_'
+        displayWord += ' '
+    }
+    wordEl.textContent = displayWord.trim()
+    if (gameStatus === 'won') {
+    messageEl.textContent = 'You won!'
+    playAgainBtn.style.display = 'block'
+  } 
+  else if (gameStatus === 'lost') {
+    messageEl.textContent = `You lost! The word was "${secretWord}."`
+    playAgainBtn.style.display = 'block'
+  } 
+  else {
+    messageEl.textContent = ''
+    playAgainBtn.style.display = 'none'
+  }
+}
+
+function createLetterButton() {
+
+}
+
+function handleMove(event) {
+  const letter = event.target.textContent;
+
+  if (gameStatus !== 'active' || guessedLetters.includes(letter)) return;
+  guessedLetters.push(letter);
+
+  if (!secretWord.includes(letter)) {
+    remainingGuesses--;
+  }
+  const isWin = secretWord.split('').every(char => guessedLetters.includes(char));
+  if (isWin) {
+    gameStatus = 'won';
+  }
+  if (remainingGuesses <= 0) {
+    gameStatus = 'lost';
+  }
+  render();
 }
